2cfe46960315f8a957e614eab3ba85fa8241e5d9
jovi20 <jovi20@users.noreply.github.com>
2026-02-05 15:24:41 +0000

    deploy: b2f923c6b97d0695def30c08ea2be34ffde2b51f

---
+++ assets/js/338e4502.91e95e52.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[1487],{8546(e){e.exports=JSON.parse('{"metadata":{"permalink":"/tradingnoobs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
