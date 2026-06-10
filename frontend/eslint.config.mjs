484fb8a1ab7f2918d038f6851e359fd773a3035f
max <jovizhjw@gmail.com>
2026-06-10 17:17:41 +0800

    chore: enable react strict hook lint globally

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,12 +2,6 @@ import nextVitals from 'eslint-config-next/core-web-vitals'
 
 const config = [
     ...nextVitals,
-    {
-        rules: {
-            'react-hooks/purity': 'off',
-            'react-hooks/set-state-in-effect': 'off',
-        },
-    },
 ]
 
 export default config
